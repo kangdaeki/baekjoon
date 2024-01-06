389b852cf08958cc6dffa0a046df274e76b0e98e
Dae-Ki Kang <dkkang@gmail.com>
2024-01-06 23:05:46 +0900

    dijkstra

--- 1753/dijkstra.py
+++ 1753/dijkstra.py
@@ -2,20 +2,23 @@ import sys
 input=sys.stdin.readline
 V,E=map(int,input().split())
 K=int(input())
+import collections
 graph=collections.defaultdict(list)
-E_set={}
 for _ in range(E):
     u,v,w=map(int,input().split())
-    if (u,v) in E_set:
+    graph[u].append((v,w))
+Q=[(0,K)]
+dist=collections.defaultdict(int)
+import heapq
+while Q:
+    time,node=heapq.heappop(Q)
+    if node not in dist:
+        dist[node]=time
+        for v,w in graph[node]:
+            alt=time+w
+            heapq.heappush(Q,(alt,v))
+for i in range(1,V+1):
+    if i in dist:
+        print(dist[i])
     else:
-        graph[u].append((v,w))
-    if (u,v) in E_set:
-        if w<E_set[(u,v)]: 
-            E_set[(u,v)]=w
-    elif (v,u) in E_set:
-        if w<E_set[(v,u)]: 
-            E_set[(v,u)]=w
-    else: 
-        E_set[(u,v)]=w
-Q={(0,K)}
-
+        print("INF")
