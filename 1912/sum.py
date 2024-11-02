f459671a178795afc0ba4f38eeca6db63beab6d3
kangdaeki <dkkang@gmail.com>
2024-11-02 19:39:23 +0900

    zz

--- 1912/sum.py
+++ 1912/sum.py
@@ -1,11 +1,11 @@
 import sys
 input=sys.stdin.readline
 N=int(input())
-if 1==N: exit()
-import math
-for i in range(2,1+int(math.sqrt(N))):
-    while 0==N%i:
-       print(i)
-       N=N//i
-if 1!=N:
-    print(N)
+num=list(map(int,input().split()))
+cost=[0 for j in range(N)]
+max_sum,cost[0]=num[0],num[0]
+for i in range(1,N):
+    cost[i]=max(num[i],num[i]+cost[i-1])
+    max_sum=max(cost[i],max_sum)
+print(max_sum)
+
